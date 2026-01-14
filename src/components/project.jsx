644d696954c505042a3c5ed4338114ae6230a8f5
feranmi-idowu <idowuferanmi4@gmail.com>
2026-01-14 23:57:00 +0100

    card animation fixed

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -34,16 +34,14 @@ function Projects() {
       <div className="container">
         <h2>Projects</h2>
 
-        <div className="projects-grid">
+        <div className="projects-grid"
+          
+        >
           {projects.map((project) => (
-            <motion
-              key={project.id}
-              
-              target="_blank"
-              rel="noopener noreferrer"
-              whileHover={{ scale: 1.05 }}
-              className="project-card"
-            >
+            <motion.div 
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              transition={{ type: "spring", stiffness: 300 }}>
               <h3>{project.title}</h3>
               <p>{project.description}</p>
               <div className="stack">
@@ -51,11 +49,25 @@ function Projects() {
                   <span key={tech}>{tech}</span>
                 ))}
               </div>
-              <div>
-                <a href={project.github}>Github link</a><Github size={18}/>
+              <div className="project-link">
+                <motion.a 
+                href={project.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                transition={{ type: "spring", stiffness: 300 }} >Github link <Github size={18}/></motion.a>
               </div>
-              <a href={project.live}>Click to view live project <ExternalLink size={18}/></a>
-            </motion>
+              <div className="project-link">
+              <motion.a href={project.live}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+                transition={{ type: "spring", stiffness: 300 }} 
+              >Click to view live project <ExternalLink size={18}/></motion.a>
+              </div>
+            </motion.div>
           ))}
         </div>
       </div>
