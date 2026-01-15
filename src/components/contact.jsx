fff367811098a4bcb6fb93ac1cbd61591832b7d7
feranmi-idowu <idowuferanmi4@gmail.com>
2026-01-15 13:20:37 +0100

    contact fixed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,10 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 
+const contacts = {
+  Github: "https://github.com/feranmi-idowu",
+  Linkedin: "https://www.linkedin.com/in/idowu-feranmi",
+  Mail: "mailto:idowuferanmi4@gmail.com",
+};
 
 function Contact() {
 return (
@@ -7,9 +12,18 @@ return (
   <h2 className="contact-title">Contact</h2>
 
   <div className="contact-links">
-    <a href="#" className="contact-link"><Github /></a>
-    <a href="#" className="contact-link"><Linkedin /></a>
-    <a href="#" className="contact-link"><Mail /></a>
+    <a  href={contacts.Github}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="contact-link"><Github /></a>
+    <a  href={contacts.Linkedin} 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="contact-link"><Linkedin /></a>
+    <a  href={contacts.Mail} 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="contact-link"><Mail /></a>
   </div>
 </section>
 );
